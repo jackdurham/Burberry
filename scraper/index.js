220421a157636d1672c1d69c4e0401fa94243969
Jack Durham <jackdurham11@gmail.com>
2018-12-14 12:24:46 +0000

    progress with on click

--- scraper/index.js
+++ scraper/index.js
@@ -1,22 +1,19 @@
 const promise = require('request-promise');
 const cheerio = require('cheerio');
-const fs = require('file-system');
+const fs      = require('file-system');
 
 //Function setting up which website I am requesting the data from
 const options = {
-  uri: `https://www.jamieoliver.com/`,
+  uri: `www.jamieoliver.com/`,
   transform: function (body) {
     return cheerio.load(body);
   }
-  // headers: {
-  //   Cookie: 'CookieControl={"necessaryCookies":["CookieControl","JSESSIONID","PHPSESSID","blaize_session","blaize_tracking_id","jo_user_login3"],"optionalCookies":{"performance":"accepted","functionality":"accepted","targetting":"accepted"},"initialState":{"type":"notify","action":"accepted"},"statement":{"shown":true,"updated":"04/09/2018"},"consentDate":1544028771895,"consentExpiry":365,"interactedWith":true,"user":"BCCFBD17-D782-4280-B83A-EA1E1AA8C12E"}'
-  // }
 };
 
 //Function making the request for data
 promise(options)
   .then($ => {
-    
+
     const homePage = $('body').html();
 
     //Function creating new HTML file and adding scraped data
@@ -30,3 +27,51 @@ promise(options)
   .catch((err) => {
     console.log(err);
   });
+
+
+        //FOR USE WITH COOKIES
+
+// const promise = require('request-promise').defaults({jar: true});
+// const cheerio = require('cheerio');
+// const fs      = require('file-system');
+// var tough     = require('tough-cookie');
+//
+// //Creating a cookie
+// let cookie = new tough.Cookie({
+//     key: "some_key",
+//     value: "some_value",
+//     domain: 'https://www.soccerstats.com/latest.asp?league=portugal',
+//     httpOnly: true,
+//     maxAge: 31536000
+// });
+//
+// //Putting cookie in a jar for multiple requests
+// var cookiejar = promise.jar();
+// cookiejar.setCookie(cookie, 'https://www.soccerstats.com/latest.asp?league=portugal');
+//
+// //Function setting up which website I am requesting the data from
+// const options = {
+//   uri: `https://www.soccerstats.com/latest.asp?league=portugal`,
+//   jar: cookiejar, //Remove for use without cookies
+//   transform: function (body) {
+//     return cheerio.load(body);
+//   }
+// };
+//
+// //Function making the request for data
+// promise(options)
+//   .then($ => {
+//
+//     const homePage = $('body').html();
+//
+//     //Function creating new HTML file and adding scraped data
+//     fs.appendFile('index.html', homePage, function (err) {
+//       if (err) throw err;
+//
+//       console.log('file created');
+//     });
+//
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
